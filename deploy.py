65fbd066849ea7f1e58dfb7918fc78f6d19c79a5
acaiblog <baocai.guo@qq.com>
2024-03-21 14:32:19 +0800

    Init

---
+++ deploy.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import argparse
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG)
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--action', type=str, required=True, help='')
+    args = parser.parse_args().__dict__
+    return args
+
+def deploy():
+    os.system('/usr/local/bin/ansible-playbook  -i inventory/hosts -e "k8s_action=%s" site.yaml' %action)
+
+if __name__ == "__main__":
+    action = args()['action']
+    locals()[action]()
